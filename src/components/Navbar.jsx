c35889660b2595f9f4b29211daca0842b5702e36
tdavis0828 <davis.t828@gmail.com>
2023-05-12 13:54:34 -0700

    add ui for the nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { StyledNavBar } from '../styles/Stylesheet';
+import logo from '../imgs/Davis.png';
 
 function Navbar() {
   return (
     <StyledNavBar>
-      <p>Navbar</p>
+      <div className="links-container">
+        <Link to="/">Home</Link>
+        <Link to="#resume">Resume</Link>
+        <Link t0="#tech-stack">Tech Stack</Link>
+        <Link to="#projects">Projects</Link>
+        <Link to="#testimonials">Testimonials</Link>
+        <Link to="#contact">Contact</Link>
+      </div>
+      <div className="logo-container">
+        <img src={logo} alt="Davis Digital Logo" />
+      </div>
     </StyledNavBar>
   );
 }
--- src/styles/GlobalStylesheet.jsx
+++ src/styles/GlobalStylesheet.jsx
@@ -1,10 +1,11 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
-    * {
+    body {
         margin: 0;
         padding: 0;
         scroll-behavior: smoooth;
         box-sizing: border-box;
+        background: #181823;
     }
 `;
