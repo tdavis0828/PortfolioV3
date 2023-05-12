c14f7884e911d180f1d225b5066e1434d7292aea
tdavis0828 <davis.t828@gmail.com>
2023-05-12 16:05:30 -0700

    build ui for the home page hero

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
 import { StyledHome } from '../styles/Stylesheet';
+import githubLogo from '../imgs/github.png';
+import fiverrLogo from '../imgs/fiverr.png';
+import linkedInLogo from '../imgs/linkedin.png';
+import headshot from '../imgs/headshot.png';
 
 function Home() {
   return (
     <StyledHome>
-      <p>Home page</p>
+      <div className="hero-content">
+        <p>My name is</p>
+        <h2>
+          <span>Tyler Davis</span>
+        </h2>
+        <p>
+          I am a <span>Web Developer</span>
+        </p>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book.{' '}
+        </p>
+        <button type="button">Check out my work</button>
+        <div className="icons-container">
+          <img src={githubLogo} alt="Github logo" />
+          <img src={linkedInLogo} alt="Linked In logo" />
+          <img src={fiverrLogo} alt="Fiverr logo" />
+        </div>
+      </div>
+      <div className="hero-image">
+        <img src={headshot} alt="headshot" />
+      </div>
     </StyledHome>
   );
 }
